82fb46fe12ab1aab5acc5f56506b0df3e4d6c497
YCY <chengyanyang0510@gmail.com>
2025-04-28 18:17:27 +0800

    fix build bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
 
 export default defineConfig({
   plugins: [
@@ -10,7 +9,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': new URL('./src', import.meta.url).pathname, // 用這種方式
     },
   },
 })
